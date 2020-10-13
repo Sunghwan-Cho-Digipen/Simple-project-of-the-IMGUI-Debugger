4c4c84e4fd992278f6eff25695b9196fac44ad85
largna <carzy1234567@gmail.com>
2020-10-13 23:11:28 +0900

    Weird problem about the logging system.

--- opengl-dev/tutorial-0/src/loggingSystem/Log.cpp
+++ opengl-dev/tutorial-0/src/loggingSystem/Log.cpp
@@ -9,29 +9,29 @@ namespace ggm
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		coreLogger = spdlog::stdout_color_mt("GGM");
-		coreLogger->set_level(spdlog::level::trace);
-		
-		clientLogger = spdlog::stdout_color_mt("APP");
-		clientLogger ->set_level(spdlog::level::info);
-		
-		//std::vector<spdlog::sink_ptr> logSinks;
-		//logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		//logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
-
-		//logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-		//logSinks[1]->set_pattern("[%T] [%l] %n: %v");
-
-		//coreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
-		//spdlog::register_logger(coreLogger);
+		//spdlog::set_pattern("%^[%T] %n: %v%$");
+		//coreLogger = spdlog::stdout_color_mt("GGM");
 		//coreLogger->set_level(spdlog::level::trace);
-		//coreLogger->flush_on(spdlog::level::trace);
+		//
+		//clientLogger = spdlog::stdout_color_mt("APP");
+		//clientLogger ->set_level(spdlog::level::info);
+		
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
 
-		//clientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
-		//spdlog::register_logger(clientLogger);
-		//clientLogger->set_level(spdlog::level::trace);
-		//clientLogger->flush_on(spdlog::level::trace);
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		coreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
+		spdlog::register_logger(coreLogger);
+		coreLogger->set_level(spdlog::level::trace);
+		coreLogger->flush_on(spdlog::level::trace);
+
+		clientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(clientLogger);
+		clientLogger->set_level(spdlog::level::trace);
+		clientLogger->flush_on(spdlog::level::trace);
 		 
 	}
 
