ca86fdfec44b65a19b9590efe8afc08b0929543e
largna <carzy1234567@gmail.com>
2020-10-14 01:00:05 +0900

    The security program was keeping the exe not change the file!!!!!!

--- opengl-dev/tutorial-0/src/loggingSystem/Log.cpp
+++ opengl-dev/tutorial-0/src/loggingSystem/Log.cpp
@@ -8,31 +8,22 @@ namespace ggm
 	std::shared_ptr<spdlog::logger> Log::clientLogger;
 
 	void Log::Init()
-	{
-		//spdlog::set_pattern("%^[%T] %n: %v%$");
-		//coreLogger = spdlog::stdout_color_mt("GGM");
-		//coreLogger->set_level(spdlog::level::trace);
-		//
-		//clientLogger = spdlog::stdout_color_mt("APP");
-		//clientLogger ->set_level(spdlog::level::info);
-		
+	{		
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("GGM.log", true));
 
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-		coreLogger = std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
+		coreLogger = std::make_shared<spdlog::logger>("GGM", begin(logSinks), end(logSinks));
 		spdlog::register_logger(coreLogger);
 		coreLogger->set_level(spdlog::level::trace);
 		coreLogger->flush_on(spdlog::level::trace);
 
-		clientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		clientLogger = std::make_shared<spdlog::logger>("Application", begin(logSinks), end(logSinks));
 		spdlog::register_logger(clientLogger);
 		clientLogger->set_level(spdlog::level::trace);
-		clientLogger->flush_on(spdlog::level::trace);
-		 
-	}
+		clientLogger->flush_on(spdlog::level::trace);	}
 
 }
